d772bf7102404e3e37c9d2496d7c2890e34cdfde
dongyuekai <457545648@qq.com>
2024-08-16 11:19:40 +0800

    feat: 控制并发（生产者每0.5秒往队列放一条消息，消费者最多并发处理3条）

--- src/consumer.js
+++ src/consumer.js
@@ -4,7 +4,25 @@ import * as amqp from 'amqplib'
 const connect = await amqp.connect(`amqp://localhost:5672`)
 const channel = await connect.createChannel()
 
+// const { queue } = await channel.assertQueue('aaa')
+// channel.consume(queue, msg => {
+//   console.log('dyk---', msg.content.toString())
+// }, { noAck: true })
+
+// 控制消费者每1s处理一条消息
 const { queue } = await channel.assertQueue('aaa')
-channel.consume(queue, msg => {
-  console.log('dyk---', msg.content.toString())
-}, { noAck: true })
+// 每次最多取回3条消息处理
+channel.prefetch(3)
+
+const currentTask = []
+channel.consume(queue, msg => { 
+  currentTask.push(msg)
+  console.log('收到消息---', msg.content.toString())
+}, { noAck: false }) // noAck设置为不自动确认 每条消费者收到的消息要确认后才会在MQ里删除，可以收到消息后自动确认，也可以手动确认
+
+setInterval(() => {
+  // 把收到的消息放入一个数组中，每 1s 确认一次
+  const curMsg = currentTask.pop()
+  channel.ack(curMsg)
+}, 1000)
+
--- src/producer.js
+++ src/producer.js
@@ -6,4 +6,13 @@ const channel = await connect.createChannel()
 
 // 创建一个aaa的队列 并向队列中发送了一个消息
 await channel.assertQueue('aaa')
-await channel.sendToQueue('aaa', Buffer.from('hello'))
+// await channel.sendToQueue('aaa', Buffer.from('hello'))
+
+// 控制并发数 生产者每0.5s发送一次消息
+let i = 1
+setInterval(async () => {
+  const msg = 'hello===' + i
+  console.log('发送消息---', msg)
+  await channel.sendToQueue('aaa', Buffer.from(msg))
+  i++
+}, 500)
